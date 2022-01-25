35ea9869aa5cbb32f1441251f80690786ddc4297
Sam James <Sam.James@softwire.com>
2022-01-25 16:18:50 +0000

    trying to combine bishop & rook functionality for queen moves

--- src/engine/pieces/queen.js
+++ src/engine/pieces/queen.js
@@ -1,4 +1,8 @@
 import Piece from './piece';
+import Rook from '../../../src/engine/pieces/rook';
+import Bishop from '../../../src/engine/pieces/bishop';
+import Player from '../../../src/engine/player';
+
 
 export default class Queen extends Piece {
     constructor(player) {
@@ -6,6 +10,11 @@ export default class Queen extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        const bishop = new Bishop(Player.WHITE);
+        const rook = new Rook(Player.WHITE);
+        let l = board.findPiece(this);
+        let BishopMoves = bishop.getAvailableMoves(board);
+        let RookMoves = rook.getAvailableMoves(board);
+        return RookMoves.push(...BishopMoves);
     }
 }
--- tests/engine/pieces/queen.spec.js
+++ tests/engine/pieces/queen.spec.js
@@ -1,5 +1,49 @@
 import Queen from '../../../src/engine/pieces/queen';
+import Board from '../../../src/engine/board';
+import Player from '../../../src/engine/player';
+import Square from '../../../src/engine/square';
 
 describe('Queen', () => {
+    let board;
+    beforeEach(() => board = new Board());
 
+    it('can move in any direction', () => {
+        const queen = new Queen(Player.WHITE);
+        board.setPiece(Square.at(2, 5), queen);
+
+        const moves = queen.getAvailableMoves(board);
+
+        const expectedMoves = [
+            // bishop moves
+            Square.at(3, 6),
+            Square.at(4, 7),
+            Square.at(1, 6),
+            Square.at(0, 7),
+            Square.at(0, 3),
+            Square.at(1, 4),
+            Square.at(7, 0),
+            Square.at(6, 1),
+            Square.at(5, 2),
+            Square.at(4, 3),
+            Square.at(3, 4),
+            // rook moves
+            Square.at(2, 0),
+            Square.at(2, 1),
+            Square.at(2, 2),
+            Square.at(2, 3),
+            Square.at(2, 4),
+            Square.at(2, 6),
+            Square.at(2, 7),
+            Square.at(0, 5),
+            Square.at(1, 5),
+            Square.at(3, 5),
+            Square.at(4, 5),
+            Square.at(5, 5),
+            Square.at(6, 5),
+            Square.at(7, 5),
+
+        ];
+
+        moves.should.deep.include.members(expectedMoves);
+    });
 });
