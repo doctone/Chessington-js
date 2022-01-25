0e5f03755ac77ad011c9c266c7d8304ae80d92ff
Sam James <Sam.James@softwire.com>
2022-01-25 14:33:16 +0000

    king movements

--- src/engine/pieces/king.js
+++ src/engine/pieces/king.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Square from '../square';
 
 export default class King extends Piece {
     constructor(player) {
@@ -6,6 +7,12 @@ export default class King extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let l = board.findPiece(this);
+        let moves = [
+            Square.at(l.row+1, l.col-1), Square.at(l.row+1, l.col), Square.at(l.row+1, l.col+1),
+                Square.at(l.row, l.col-1), Square.at(l.row, l.col+1),
+            Square.at(l.row-1, l.col-1), Square.at(l.row-1, l.col), Square.at(l.row-1, l.col+1)
+        ]
+        return moves;
     }
 }
