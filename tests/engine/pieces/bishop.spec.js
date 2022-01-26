f90d1f6e5dfed493b479308a598cce044be5556c
Sam James <Sam.James@softwire.com>
2022-01-26 15:45:22 +0000

    added test for bishops movement scope

--- tests/engine/pieces/bishop.spec.js
+++ tests/engine/pieces/bishop.spec.js
@@ -3,6 +3,7 @@ import Bishop from '../../../src/engine/pieces/bishop';
 import Board from '../../../src/engine/board';
 import Player from '../../../src/engine/player';
 import Square from '../../../src/engine/square';
+import Knight from '../../../src/engine/pieces/knight.js'
 
 describe('Bishop', () => {
     
@@ -56,6 +57,29 @@ describe('Bishop', () => {
         ];
 
         moves.should.deep.include.members(expectedMoves);
+    });
+
+    it('cannot move beyond another piece', () => {
+        board.setPiece(Square.at(2, 5), bishop);
+        const knight = new Knight(Player.WHITE);
+
+        board.setPiece(Square.at(3, 6), knight);
+
+        const moves = bishop.getAvailableMoves(board);
+
+        const expectedMoves = [
+            Square.at(1, 6),
+            Square.at(0, 7),
+            Square.at(0, 3),
+            Square.at(1, 4),
+            Square.at(7, 0),
+            Square.at(6, 1),
+            Square.at(5, 2),
+            Square.at(4, 3),
+            Square.at(3, 4),
+        ]
+
+        moves.should.deep.include(expectedMoves);
     });
 
 });
