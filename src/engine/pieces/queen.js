944752b51fac6fc3354f1681119145853dc1725a
Sam James <Sam.James@softwire.com>
2022-01-26 16:16:33 +0000

    queen working

--- src/engine/pieces/queen.js
+++ src/engine/pieces/queen.js
@@ -3,6 +3,7 @@ import Rook from '../../../src/engine/pieces/rook';
 import Bishop from '../../../src/engine/pieces/bishop';
 import Player from '../../../src/engine/player';
 import Square from '../../../src/engine/square';
+import Board from '../board';
 
 
 export default class Queen extends Piece {
@@ -11,19 +12,26 @@ export default class Queen extends Piece {
     }
 
     getAvailableMoves(board) {
-        const bishop = new Bishop(Player.WHITE);
-        const rook = new Rook(Player.WHITE);
+        let bishop;
+        let rook;
+
+        bishop = this.player == Player.WHITE 
+        ? new Bishop(Player.WHITE) : new Bishop(Player.BLACK);
+        rook = this.player == Player.WHITE 
+        ? new Rook(Player.WHITE) : new Rook(Player.BLACK);
+
         let l = board.findPiece(this);
 
         // put bishop on the same square and get its moves
-        board.setPiece(Square.at(l.row, l.col), bishop);
-        let BishopMoves = bishop.getAvailableMoves(board);
+        let board1 = new Board(this.player);
+        board1.setPiece(Square.at(l.row, l.col), bishop);
+        let BishopMoves = bishop.getAvailableMoves(board1);
         // put rook on the same square and get its moves
-        board.setPiece(Square.at(l.row, l.col), rook);
-        let RookMoves = rook.getAvailableMoves(board);
+        board1.setPiece(Square.at(l.row, l.col), rook);
+        let RookMoves = rook.getAvailableMoves(board1);
 
         //combine moves
         let moves = RookMoves.concat(BishopMoves);
-        return moves;
+        return moves; 
     }
 }
