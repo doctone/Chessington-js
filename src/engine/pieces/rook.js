ad848dae3aa078603b8f06421d9c813b8d2e298f
Sam James <Sam.James@softwire.com>
2022-01-27 10:03:24 +0000

    rook cannot move beyond other pieces

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -8,19 +8,48 @@ export default class Rook extends Piece {
 
     getAvailableMoves(board) {
         let location = board.findPiece(this)
-        let moves = []
-        for (let i=0; i<8; i++){
-            if (i!==location.col){
-                moves.push(
-                    Square.at(location.row, i)
-                )
-            }
-            if(i!==location.row){
-                moves.push(
-                    Square.at(i, location.col)
-                )
+        let availableMoves = []
+        for (let i=location.row+1; i<8; i++) { // vertically up
+            if (board.getPiece(Square.at(i, location.col))===undefined) //can move only until it meet another piece
+                availableMoves.push(Square.at(i, location.col));
+            else {
+                //if it met an opponent's piece, it can capture it
+                if (!this.sameColour(board,Square.at(i, location.col)))
+                    availableMoves.push(Square.at(i, location.col));
+                break;
             }
         }
-        return moves
+        for (let i=location.row-1; i>=0; i--) { // vertically down
+            if (board.getPiece(Square.at(i, location.col))===undefined)
+                availableMoves.push(Square.at(i, location.col));
+            else {
+                //if it met an opponent's piece, it can capture it
+                if (!this.sameColour(board,Square.at(i, location.col)))
+                    availableMoves.push(Square.at(i, location.col));
+                break;
+                }
+        }
+        for (let i=location.col+1; i<8; i++) { // horizontally right
+            if (board.getPiece(Square.at(location.row, i))===undefined)
+                availableMoves.push(Square.at(location.row, i));
+            else {
+                //if it met an opponent's piece, it can capture it
+                if (!this.sameColour(board,Square.at(location.row, i)))
+                    availableMoves.push(Square.at(location.row, i));
+                break;
+                }
+        }
+        for (let i=location.col-1; i>=0; i--) { // horizontally left
+            if (board.getPiece(Square.at(location.row, i))===undefined)
+                availableMoves.push(Square.at(location.row, i));
+            else {
+                //if it met an opponent's piece, it can capture it
+                if (!this.sameColour(board,Square.at(location.row, i)))
+                    availableMoves.push(Square.at(location.row, i));
+                break;
+                }
+        }
+        //console.log(availableMoves);
+        return availableMoves;
     }
 }
